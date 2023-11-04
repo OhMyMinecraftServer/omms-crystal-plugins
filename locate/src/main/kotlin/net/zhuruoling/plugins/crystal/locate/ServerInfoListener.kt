b41bc958bb2c95f52e5d7bee344cae145adb2e98
ZhuRuoLing <jkl-963@live.com>
2023-11-04 20:38:22 +0800

    use java to write plugin is a big mistake

--- locate/src/main/kotlin/net/zhuruoling/plugins/crystal/locate/ServerInfoListener.kt
+++ locate/src/main/kotlin/net/zhuruoling/plugins/crystal/locate/ServerInfoListener.kt
@@ -1,12 +1,13 @@
 package net.zhuruoling.plugins.crystal.locate
 
+import net.zhuruoling.omms.crystal.event.ServerInfoEvent
 import net.zhuruoling.omms.crystal.event.ServerInfoEventArgs
 import net.zhuruoling.omms.crystal.plugin.api.annotations.EventHandler
 
 class ServerInfoListener {
 
 
-    @EventHandler(event = "")
+    @EventHandler(event = ServerInfoEvent::class)
     fun onInfo(e: ServerInfoEventArgs){
         val info = e.info.info
 
